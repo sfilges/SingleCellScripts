d2c50ad76d02340b65da723806db8a890dca339f
ozimand1as <filges.stefan@gmail.com>
2020-06-11 16:23:13 +0200

    update

--- R/6_cytoscape_networks.R
+++ R/6_cytoscape_networks.R
@@ -3,26 +3,23 @@ if(!"RCy3" %in% installed.packages()){
   BiocManager::install("RCy3")
 }
 
-
 library(tidyverse)
 library(RColorBrewer)
 library(RCy3)
 
+# define the working directory
+working_directory <- "~/GitHub/SingleCellScripts/"
+
 cytoscapePing()
 cytoscapeVersionInfo()
 
-top_genes <- read_csv2("~/GitHub/SingleCellScripts/data/gene_lists/diff_genes_over_pseudotime_top_1500_q_0.01.csv")
-
-#top_genes <- dplyr::filter(top_genes, cluster == 2)
+top_genes <- read_csv2(paste(working_directory, "data/gene_lists/diff_genes_over_pseudotime_top_1500_q_0.01.csv", sep = ""))
 
 string.cmd = paste('string protein query cutoff=0.4 species="Homo sapiens" limit=0 query="',
   paste(top_genes$gene, collapse=","),'"',sep="")
 
 commandsGET(string.cmd)
-#commandsRun(string.cmd)
-
 commandsRun("analyzer analyze directed=FALSE")
-
 
 loadTableData(
   as.data.frame(top_genes),
@@ -48,17 +45,6 @@ setNodeLabelMapping('display name', style.name)
 setNodeSizeMapping ('BetweennessCentrality', control.points, sizes, style.name = style.name)
 setEdgeLineWidthDefault(0.0001, style.name = style.name)
 setNodeColorMapping("cluster", c(1,2,3,4), node.colors, style.name=style.name)
-
-# Save network
-full_string_network_pdf_file_name <- file.path("~/GitHub/SingleCellScripts/figures/networks", "full_string_network.pdf")
-
-if(file.exists(full_string_network_pdf_file_name)){
-  #cytoscape hangs waiting for user response if file already exists.  Remove it first
-  response <- file.remove(full_string_network_pdf_file_name)
-}
-
-response <- exportImage("full_string_network.pdf", type = "pdf")
-
 
 #---------------------// Generating sub networks //----------------------
 
@@ -94,8 +80,8 @@ for(i in clusters){
   setNodeColorMapping("cluster", i, node.color, style.name=style.name)
   
   
-  file.name <- paste("string_network_cluster_",i,".pdf", sep="")
-  response <- exportImage(file.name, type = "pdf")
+  file.name <- paste(working_directory, "figures/string_network_cluster_",i,".pdf", sep="")
+  #response <- exportImage(file.name, type = "pdf")
   
 }
 
